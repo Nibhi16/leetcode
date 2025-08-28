d120bb3b73a10ab907166183c7bc06dce321f857
Nibhi16 <gargnibhi@gmail.com>
2025-08-28 15:09:20 +0530

    Time: 0 ms (100%), Space: 10 MB (66.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,14 +4,21 @@ public:
         int n=s1.size();
         int m=s2.size();
         if(n>m) return false;
-        vector<int> freq1(26,0);
-        for(char c : s1) freq1[c - 'a']++;
-        for(int i=0;i<=m-n;i++){
-            string sub= s2.substr(i,n);
-            vector<int> freq2(26,0);
-            for(char c : sub) freq2[c - 'a']++;
-                if( freq1 == freq2)
-                return true;
+        vector<int> freq1(26);
+        vector<int> freq2(26);
+        for(char c : s1){
+            freq1[c - 'a']++;
+        }
+        int i=0;
+        int j=0;
+        while(j<m){
+            freq2[s2[j]-'a']++;
+            if(j-i+1>n){
+                freq2[s2[i]-'a']--;
+                i++;
+            }
+            if(freq1==freq2) return true;
+            j++;
         }
         return false;
     }
