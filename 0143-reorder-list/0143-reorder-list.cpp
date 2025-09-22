71c7d25e9be7d53b0bacf17ace1a65e52232201a
Nibhi16 <gargnibhi@gmail.com>
2025-09-22 16:39:30 +0530

    Time: 0 ms (100%), Space: 22.9 MB (35.74%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,20 +11,36 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-       vector<ListNode*> nodes;
-       ListNode* curr = head;
-       while(curr){
-        nodes.push_back(curr);
-        curr= curr->next;
-       } 
-       int i=0; int j=nodes.size()-1;
-       while(i<j){
-        nodes[i]->next= nodes[j];
-        i++;
-        if ( i==j ) break;
-        nodes[j]->next= nodes[i];
-        j--;
-       }
-       nodes[i]->next=NULL;
+        ListNode* slow= head;
+        ListNode* fast = head;
+        while ( fast->next && fast->next->next ){
+            slow= slow->next;
+            fast= fast->next->next;
+        }
+        //reverse list
+        ListNode* prev= NULL;
+        ListNode* curr= slow->next;
+        while(curr){
+            ListNode* nextTemp = curr->next;
+            curr->next= prev;
+            prev= curr;
+            curr=nextTemp;
+        }
+        slow->next= NULL;
+        //merge alternatively
+        ListNode* first= head;
+        ListNode* second=prev;
+        while(second){
+            ListNode* tmp1= first->next;
+            ListNode* tmp2=second->next;
+
+            first->next=second;
+            second->next=tmp1;
+
+            first=tmp1;
+            second=tmp2;
+
+        }
+
     }
 };
