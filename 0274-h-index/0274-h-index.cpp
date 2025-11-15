83827e3a17c4adff53a6d45a003f68e995c992fb
Nibhi16 <gargnibhi@gmail.com>
2025-11-15 21:43:28 +0530

    Time: 0 ms (100%), Space: 13.8 MB (5.33%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(), citations.rend()); // sort descending
+        int h = 0;
+        for (int i = 0; i < citations.size(); i++) {
+            if (citations[i] >= i + 1)
+                h = i + 1;
+            else
+                break;
+        }
+        return h;
+    }
+};
